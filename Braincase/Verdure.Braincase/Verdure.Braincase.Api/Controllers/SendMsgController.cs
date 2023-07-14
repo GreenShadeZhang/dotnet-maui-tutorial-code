71a03fa53fbc8f3e6ece915596a81c267e39e2ce
gil zhang <gil.zhang.dev@outlook.com>
2023-07-14 23:48:07 +0800

    添加测试项目

---
+++ Braincase/Verdure.Braincase/Verdure.Braincase.Api/Controllers/SendMsgController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Verdure.Braincase.Api.Services;
+using Verdure.Common;
+
+namespace Verdure.Braincase.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SendMsgController : ControllerBase
+    {
+        private readonly IHubContext<ChatHub> _hubContext;
+
+
+        private readonly ILogger<SendMsgController> _logger;
+
+        public SendMsgController(ILogger<SendMsgController> logger, IHubContext<ChatHub> hubContext)
+        {
+            _logger = logger;
+            _hubContext = hubContext;
+        }
+
+
+        [HttpPost(Name = "SendMsg")]
+        public async Task SendMsgAsync(MsgModel msgModel)
+        {
+            await _hubContext.Clients.All.SendAsync("ReceiveMessage", msgModel);
+        }
+    }
+}
--- Braincase/Verdure.Braincase/Verdure.Braincase/MainPage.xaml.cs
+++ Braincase/Verdure.Braincase/Verdure.Braincase/MainPage.xaml.cs
@@ -1,13 +1,50 @@
-﻿namespace Verdure.Braincase;
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Maui.Media;
+using Verdure.Common;
+
+namespace Verdure.Braincase;
 
 public partial class MainPage : ContentPage
 {
 	int count = 0;
+    HubConnection connection;
 
-	public MainPage()
+    public MainPage()
 	{
 		InitializeComponent();
         NavigationPage.SetHasNavigationBar(this, false);
+
+
+        connection = new HubConnectionBuilder()
+            .WithUrl("http://192.168.3.221:5241/Chat")
+            .Build();
+
+        connection.Closed += async (error) =>
+        {
+            await Task.Delay(new Random().Next(0, 5) * 1000);
+            await connection.StartAsync();
+        };
+    }
+
+    private async void MainPage_OnLoaded(object sender, EventArgs e)
+    {
+        await TextToSpeech.SpeakAsync("你好世界");
+        connection.On<MsgModel>("ReceiveMessage", async (msgModel) =>
+        {
+            if (msgModel != null)
+            {
+                await TextToSpeech.SpeakAsync(msgModel.TtsText);
+            }
+        });
+
+        try
+        {
+            await connection.StartAsync();
+        }
+        catch (Exception ex)
+        {
+        }
+        
     }
 }
 
